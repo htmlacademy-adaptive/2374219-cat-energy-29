e0d05f08a1d8671822321ba1919faa4daa09249a
Talgat <0_talgat_0@mail.ru>
2023-09-16 20:22:22 +0500

    Исправление ошибок

--- source/js/script.js
+++ source/js/script.js
@@ -19,3 +19,76 @@ navToggle.addEventListener('click', function () {
   }
 
 });
+
+const slider = document.querySelector('.slider');
+const before = document.querySelector('.slider__cat--fat');
+const beforeImage = document.querySelector('.example__image-cat--fat');
+const button = document.querySelector('.slider__button');
+const body = document.body;
+
+let isActive = false;
+
+button.addEventListener('mousedown', () => {
+	isActive = true;
+});
+
+body.addEventListener('mouseup', () => {
+	isActive = false;
+});
+
+body.addEventListener('mouseleave', () => {
+	isActive = false;
+});
+
+const beforeAfterSlider = (x) => {
+	let shift = Math.max(0, Math.min(x, slider.offsetWidth));
+	before.style.width = `${shift}px`;
+	button.style.left = `${shift}px`;
+};
+
+const pauseEvents = (e) => {
+	e.stopPropagation();
+	e.preventDefault();
+	return false;
+};
+
+body.addEventListener('mousemove', (e) => {
+	if (!isActive) {
+		return;
+	}
+
+	let x = e.pageX;
+	x -= slider.getBoundingClientRect().left;
+	beforeAfterSlider(x);
+	pauseEvents(e);
+});
+
+button.addEventListener('touchstart', () => {
+	isActive = true;
+});
+
+body.addEventListener('touchend', () => {
+	isActive = false;
+});
+
+body.addEventListener('touchcancel', () => {
+	isActive = false;
+});
+
+body.addEventListener('touchmove', (e) => {
+	if (!isActive) {
+		return;
+	}
+
+  let x;
+
+  let i;
+  for (i = 0; i < e.buttondTouches.length; i++) {
+  	x = e.buttondTouches[i].pageX;
+  }
+
+  x -= slider.getBoundingClientRect().left;
+
+  beforeAfterSlider(x);
+  pauseEvents(e);
+});
