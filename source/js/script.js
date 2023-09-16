b51c1539e945c1c914d761b878a649f8009717d6
Talgat <0_talgat_0@mail.ru>
2023-09-16 20:31:38 +0500

    Доработка

--- source/js/script.js
+++ source/js/script.js
@@ -29,62 +29,62 @@ const body = document.body;
 let isActive = false;
 
 button.addEventListener('mousedown', () => {
-	isActive = true;
+  isActive = true;
 });
 
 body.addEventListener('mouseup', () => {
-	isActive = false;
+  isActive = false;
 });
 
 body.addEventListener('mouseleave', () => {
-	isActive = false;
+  isActive = false;
 });
 
 const beforeAfterSlider = (x) => {
-	let shift = Math.max(0, Math.min(x, slider.offsetWidth));
-	before.style.width = `${shift}px`;
-	button.style.left = `${shift}px`;
+  let shift = Math.max(0, Math.min(x, slider.offsetWidth));
+  before.style.width = `${shift}px`;
+  button.style.left = `${shift}px`;
 };
 
 const pauseEvents = (e) => {
-	e.stopPropagation();
-	e.preventDefault();
-	return false;
+  e.stopPropagation();
+  e.preventDefault();
+  return false;
 };
 
 body.addEventListener('mousemove', (e) => {
-	if (!isActive) {
-		return;
-	}
+  if (!isActive) {
+    return;
+  }
 
-	let x = e.pageX;
-	x -= slider.getBoundingClientRect().left;
-	beforeAfterSlider(x);
-	pauseEvents(e);
+  let x = e.pageX;
+  x -= slider.getBoundingClientRect().left;
+  beforeAfterSlider(x);
+  pauseEvents(e);
 });
 
 button.addEventListener('touchstart', () => {
-	isActive = true;
+  isActive = true;
 });
 
 body.addEventListener('touchend', () => {
-	isActive = false;
+  isActive = false;
 });
 
 body.addEventListener('touchcancel', () => {
-	isActive = false;
+  isActive = false;
 });
 
 body.addEventListener('touchmove', (e) => {
-	if (!isActive) {
-		return;
-	}
+  if (!isActive) {
+    return;
+  }
 
   let x;
 
   let i;
   for (i = 0; i < e.buttondTouches.length; i++) {
-  	x = e.buttondTouches[i].pageX;
+    x = e.buttondTouches[i].pageX;
   }
 
   x -= slider.getBoundingClientRect().left;
